cab6892b75694a286cd9fc1c99e3713923967315
Justin <justin.esters.23@cmc.edu>
2021-02-23 19:24:36 -0800

    Changes

--- word_ladder.py
+++ word_ladder.py
@@ -1,54 +1,74 @@
-#!/bin/python3
+from collections import deque
+import copy
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
-    '''
-    Returns a list satisfying the following properties:
 
-    1. the first element is `start_word`
-    2. the last element is `end_word`
-    3. elements at index i and i+1 are `_adjacent`
-    4. all elements are entries in the `dictionary_file` file
+    with open(dictionary_file) as f:
+        new_dictionary = f.readlines()
 
-    For example, running the command
-    ```
-    word_ladder('stone','money')
-    ```
-    may give the output
-    ```
-    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
-    ```
-    but the possible outputs are not unique,
-    so you may also get the output
-    ```
-    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
-    ```
-    (We cannot use doctests here because the outputs are not unique.)
+    dictionary = []
+    for element in new_dictionary:
+        dictionary.append(element.strip())
 
-    Whenever it is impossible to generate a word ladder between the two words,
-    the function returns `None`.
-    '''
+    if start_word == end_word:
+        return [start_word]
+    if len(start_word) != len(end_word):
+        return None
+    if _adjacent(start_word, end_word):
+        return [start_word, end_word]
+
+    stack = []
+    stack.append(start_word)
+    dictionary.remove(start_word)
+    queue = deque([])
+    queue.append(stack)
+
+    while len(queue) != 0:
+        stack2 = queue.popleft()
+        dictionary_copy = copy.deepcopy(dictionary)
+        for word in dictionary_copy:
+            if _adjacent(word, stack2[-1]):
+                if word == end_word:
+                    stack2.append(word)
+                    return stack2
+                new_stack = copy.deepcopy(stack2)
+                new_stack.append(word)
+                queue.append(new_stack)
+                dictionary.remove(word)
+    return None
 
 
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
-
     >>> verify_word_ladder(['stone', 'shone', 'phone', 'phony'])
     True
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+    if len(ladder) == 0:
+        return False
+    for num in range(len(ladder) - 1):
+        if not _adjacent(ladder[num], ladder[num + 1]):
+            return False
+    return True
 
 
 def _adjacent(word1, word2):
     '''
     Returns True if the input words differ by only a single character;
     returns False otherwise.
-
     >>> _adjacent('phone','phony')
     True
     >>> _adjacent('stone','money')
     False
     '''
+    if (len(word1) != len(word2)):
+        return False
+    change = 0
+    for i in range(0, min(len(word1), len(word2))):
+        if word1[i] != word2[i]:
+            change += 1
+    return change == 1
